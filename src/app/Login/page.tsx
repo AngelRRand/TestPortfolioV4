f221ac26e64a760f23e73d1cc1eed3227617d676
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-08-24 23:52:57 -0300

    Set Icons

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import React, { useState } from 'react'
 import styles from './Login.module.scss';
 import Stars from '@/src/component/stars/Stars';
+import Image from 'next/image';
 
 export default function page() {
 	const [input, setinput] = useState("")
@@ -15,7 +16,25 @@ export default function page() {
 			<Stars />
 			<section>
 				<div>
-					
+
+				<Image
+					width={500}
+					height={500}
+					alt='circule'
+					src="/assets/Circulo.svg"
+				/>
+				</div>
+				<div>
+					{/* 
+					<Image
+						width={100}
+						height={100}
+						alt='Estrellas'
+						src="/assets/Stars.gif"
+						className={styles.stars}
+						priority={true}
+					/> 
+					*/}
 				</div>
 			</section>
 			<section>
