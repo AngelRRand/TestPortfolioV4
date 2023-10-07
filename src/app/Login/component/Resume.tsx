b2fc8fe289cecdc41a27686023177e25ef53244f
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-10-06 21:46:07 -0300

    fixed bug types

--- src/app/Login/component/Resume.tsx
+++ src/app/Login/component/Resume.tsx
@@ -1,13 +1,15 @@
-import SpriteBox from '@/src/component/sprite/SpriteBox'
 import React from 'react'
 import styles from './Resume.module.scss';
+import {useRouter} from "next/navigation";
+import Link from "next/link";
 
 const Resume = () => {
-  return (
-    <div className={styles.container}>
-
-    </div>
-  )
+    const router = useRouter()
+    return (
+        <div className={styles.container}>
+            <Link href={`/Casa/${30}`}>Mover</Link>
+        </div>
+    )
 }
 
 export default Resume
