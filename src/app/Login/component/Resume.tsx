070515a51e04f266add532f0aa62390e0dee18f9
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-10-23 20:35:00 -0300

    finish deploy

--- src/app/Login/component/Resume.tsx
+++ src/app/Login/component/Resume.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import styles from './Resume.module.scss';
-import {useRouter} from "next/navigation";
-import Link from "next/link";
 
 const Resume = () => {
-    const router = useRouter()
     return (
         <div className={styles.container}>
-            <Link href={`/Casa/${30}`}>Mover</Link>
         </div>
     )
 }
