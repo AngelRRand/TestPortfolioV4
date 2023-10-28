96a1c089f2eb7f8effb7428960a4ef01370bf9d8
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-10-28 12:06:24 -0300

    Home links Structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
+import './globals.css'
 import React, {useEffect} from "react";
 import Image from "next/image";
+import Link from "next/link";
 import useLocalStorage from "../hooks/useLocalStorage";
 import {redirect} from "next/navigation";
 import ContainerPage from "@/src/component/container/ContainerPage";
 import styles from './Home.module.scss';
-import './globals.css'
 
 export default function Home() {
     const [name, setName] = useLocalStorage('name', '');
@@ -41,7 +42,24 @@ export default function Home() {
                 />
             </div>
             <div className={styles.containerGalaxyGrid}>
-                <div className={styles.casa}></div>
+                <Link href={'/About'} className={`${styles.home_link} ${styles.home_about}`}>
+                    <Image
+                        src={"/assets/home/planetTierra.png"}
+                        alt='Planet Terra galaxy link image'
+                        className='pointer'
+                        width={30}
+                        height={30}
+                        layout='responsive'
+                    />
+                    <span className='text_señal'>About</span>
+                    <Image
+                        src={"/assets/home/Señal.gif"}
+                        alt='Pointer galaxy'
+                        className='pointer señal'
+                        width={80}
+                        height={80}
+                    />
+                </Link>
             </div>
         </ContainerPage>
     )
